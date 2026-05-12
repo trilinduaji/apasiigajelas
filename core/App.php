c17c6d38cf346fb5ccd9dfefdfb5c1c774141b56
Tri Lindu Aji <trilinduaji11@gmail.com>
2026-05-12 16:11:21 +0700

    Update App.php

--- core/App.php
+++ core/App.php
@@ -1,1 +1,34 @@
+<?php
+/**
+ * SIPEDO - Core Application Bootstrap
+ * Handles routing and MVC dispatch
+ */
+class App {
+    private static string $basePath;
+
+    public static function init(string $basePath): void {
+        self::$basePath = rtrim($basePath, '/');
+        spl_autoload_register([self::class, 'autoload']);
+    }
+
+    private static function autoload(string $class): void {
+        $dirs = [
+            self::$basePath . '/app/Controllers/',
+            self::$basePath . '/app/Models/',
+            self::$basePath . '/config/',   // DB class
+            self::$basePath . '/core/',
+        ];
+        foreach ($dirs as $dir) {
+            $file = $dir . $class . '.php';
+            if (file_exists($file)) {
+                require_once $file;
+                return;
+            }
+        }
+    }
+
+    public static function basePath(): string {
+        return self::$basePath;
+    }
+}
 
